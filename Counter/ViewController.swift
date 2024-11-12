0391bf92d291b369fb76c1b6d5371ca1005e930b
Victor <ondewhfe@yandex.kz>
2024-11-12 18:50:21 +0500

    Implement hard version.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var history: UITextView!
+    
+    private enum ActionType {
+        case decrement
+        case invalidDecrement
+        case increment
+        case reset
+    }
+    
+    private var counterValue: Int = 0 {
+        didSet {
+            label.text = "Значение счетчика: \(counterValue)"
+        }
+    }
+    
+    @IBAction func decrementButtonDidTap() {
+        let action: ActionType = counterValue > 0 ? .decrement : .invalidDecrement
+        handleAction(action)
+    }
+    
+    @IBAction func incrementButtonDidTap() {
+        handleAction(.increment)
+    }
+    
+    @IBAction func resetButtonDidTap() {
+        handleAction(.reset)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    private func handleAction(_ action: ActionType) {
+        let currentTime = Date().formatted(date: .numeric, time: .shortened)
+        
+        var change = "\n\(currentTime): "
+        switch action {
+        case .decrement:
+            counterValue -= 1
+            change += "значение изменено на -1"
+        case .invalidDecrement:
+            change += "попытка уменьшить значение счетчика ниже 0"
+        case .increment:
+            counterValue += 1
+            change += "значение изменено на +1"
+        case .reset:
+            counterValue = 0
+            change += "значение сброшено"
+        }
+        
+        history.text.append(change)
+    }
 }
-
