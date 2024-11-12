8d3af37d3aa3b378a33851e83f365db53a2b1dc1
Victor <ondewhfe@yandex.kz>
2024-11-12 17:57:35 +0500

    Create project.

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Victor Kim on 12.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
