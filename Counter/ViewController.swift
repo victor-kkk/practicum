d0bc5a4456a4c334b8ca4145fd3d4e09977977b2
Victor <ondewhfe@yandex.kz>
2024-11-13 14:03:57 +0500

    Make ViewController class final.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private enum ActionType {
         case decrement
         case invalidDecrement
